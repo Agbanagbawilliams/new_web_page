bf89befed9cfe9c8c93316e8805f8882f9540881
williamsagbanagba <agbanagbawilliams@gmail.com>
2024-07-27 10:31:37 -0400

    test web app

---
+++ pages/3_Chatbot.py
@@ -0,0 +1,44 @@
+import random
+import time
+
+import streamlit as st
+
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hey there! Need help to make a chatbot? click here 'https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps'",
+            "Hi need ideas for coding in scratch click here https://scratch.mit.edu/ideas",
+            "Hello! want to see my scratch projects click here https://scratch.mit.edu/users/Williamsagb/projects/",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("Chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
