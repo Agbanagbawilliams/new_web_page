65d17850064d4f8754f9fa2bb154478fbc966296
Williams <agbanagbawilliams@gmail.com>
2024-08-24 18:36:17 -0400

    Update 👋Homepage.py

--- 👋Homepage.py
+++ 👋Homepage.py
@@ -12,14 +12,14 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 # Nav bar
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" target="_blank">Multi app</a>
+  <a class="navbar-brand" href"https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/" target="_blank">Multi app</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled">href="https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/" Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/" target="_blank">Project</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/Project" target="_blank">Project</a>
