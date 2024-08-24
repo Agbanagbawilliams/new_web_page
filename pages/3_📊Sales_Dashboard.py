0e9e4f2f96a71a109accc2287cd0bf108f443a26
williamsagbanagba <agbanagbawilliams@gmail.com>
2024-08-24 13:23:12 -0400

    test web-app

---
+++ pages/3_📊Sales_Dashboard.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import replicate
+
+st.title("📊Sales Dashboard")
+
+st.subheader("Excel sheet")
+def load_data(path: str):
+    data = pd.read_excel(path)
+    return data
+upload_file = st.file_uploader("Chose a file")
+if upload_file is None:
+
+    df = load_data("Work.xlsx")
+    st.dataframe(df)
+
+st.write("---")
+st.subheader("Bar Chart")
+
+def Data(path: str):
+    bar = pd.read_excel(path)
+    return bar
+i = load_data("Work.xlsx")
+st.bar_chart(i)
