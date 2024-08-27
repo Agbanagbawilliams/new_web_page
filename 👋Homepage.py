0db11a2fb3d5dd4f27f83ae8bdffc7eb9f42ba57
Williams <agbanagbawilliams@gmail.com>
2024-08-27 08:00:00 -0400

    Update 👋Homepage.py

--- 👋Homepage.py
+++ 👋Homepage.py
@@ -11,7 +11,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #E50900">
-  <a class="navbar-brand" target="_blank">Multi app</a>
+  <a class="navbar-brand" target="_blank">Neft</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
