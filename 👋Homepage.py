fc059ec37d79060c5a4660c2da5942c05bb99dd0
williamsagbanagba <agbanagbawilliams@gmail.com>
2024-09-14 21:30:14 -0400

    test web-app vd

--- 👋Homepage.py
+++ 👋Homepage.py
@@ -10,17 +10,15 @@ st.set_page_config(
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #E50900">
-  <a class="navbar-brand" target="_blank">Multi app</a>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #00B4F0">
+  <a class="navbar-brand" target="_blank">Mech</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
      <li class="nav-item active">
-        <a class="nav-link" href="https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/" target="_blank">About me</a>
-      <li class="nav-item active">
-        <a class="nav-link" href="https://agbanagbawilliams-mywebpage-app-vqxbm4.streamlit.app/" target="_blank">Project</a>
+        <a class="nav-link" href="https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/" target="_blank">Home</a>
       <li class="nav-item">
         <a class="nav-link" href="https://blank-app-quobxypwoytpuhz3cchas6.streamlit.app/" target="_blank">Chatbot</a>
       </li>
@@ -66,14 +64,27 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # ----CONTACT ME ----
-st.write("---")
-st.header("Contact Me📬")
+import streamlit as st  # pip install streamlit
 
-st.text_input("First Name")
-st.text_input("Last Name")
-number = st.slider("Age", min_value=0, max_value=100)
-st.text_input("Email")
-st.text_input("Message")
-submit_button = st.button("Submit")
-if submit_button:
-    st.success("Message successfully sent")
+st.header(":mailbox: Get In Touch With Me!")
+
+
+contact_form = """
+<form action="https://formsubmit.co/agbanagbawilliams@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
